0247ad8910714445181c0a78316a79f625c8815e
janus-zheng <lilei.zheng@tokopedia.com>
2023-04-05 13:24:19 +0000

    test u2netp onnx

--- zll_test_u2netp_onnx.py
+++ zll_test_u2netp_onnx.py
@@ -8,7 +8,7 @@ from imgcat import imgcat
 import cv2
 import torch
 from torch.autograd import Variable
-from model import U2NET
+from model import U2NETP
 
 
 # Change shapes and types to match model
@@ -21,7 +21,7 @@ img_arr = np.transpose(img_arr, [2, 0, 1])
 img_arrs = np.expand_dims(img_arr, axis=0).astype(np.float32)
 print(img_arrs.shape)
 
-sess = ort.InferenceSession("./u2net.onnx", providers=["CPUExecutionProvider"])
+sess = ort.InferenceSession("./u2netp.onnx", providers=["CPUExecutionProvider"])
 
 # Set first argument of sess.run to None to use all model outputs in default order
 # Input/output names are printed by the CLI and can be set with --rename-inputs and --rename-outputs
@@ -33,8 +33,8 @@ for _ in range(20):
     results_ort = sess.run(res_keys, {"input": img_arrs})
     print(f"time cost: {time.time()-since:.4f} seconds")
 
-model = U2NET(3, 1)
-model_path = "./saved_models/u2net/u2net.pth"
+model = U2NETP(3, 1)
+model_path = "./saved_models/u2netp/u2netp.pth"
 model.load_state_dict(torch.load(model_path, map_location='cpu'))
 model.eval()
 
@@ -47,6 +47,6 @@ for _ in range(20):
     print(f"time cost: {time.time()-since:.4f} seconds")
 
 for ort_res, tf_res in zip(results_ort[0], d1.detach().numpy()):
-    np.testing.assert_allclose(ort_res, tf_res, rtol=1e-5, atol=1e-5)
+    np.testing.assert_allclose(ort_res, tf_res, rtol=2e-5, atol=2e-5)
 
 print("Results match")
